eb178a3564e3b09340135e555d845c88f5ab6f88
p1198528948 <690756581@qq.com>
2019-05-05 13:44:26 +0800

    ---

---
+++ project_complicated/complicated.h
@@ -0,0 +1,8 @@
+// ---------------------------------------------
+// complicated.h
+#ifndef __COMPLICATED_H__
+#define __COMPLICATED_H__
+#define HELLO_STRING "Hello !"
+#define PROJECT_NAME "complicated"
+extern void complicated(void);
+#endif
---
+++ project_complicated/main.c
@@ -0,0 +1,10 @@
+// ---------------------------------------------
+// main.c
+#include <stdio.h>
+#include "complicated.h"
+int main()
+{
+    printf("%s\n", HELLO_STRING);
+    complicated();
+    return 0;
+}
